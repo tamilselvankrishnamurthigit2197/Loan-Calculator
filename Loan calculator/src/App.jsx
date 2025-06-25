5683b3897ff68a2abc6a1f3b90cf114a127ba9fb
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-06-25 13:45:44 +0530

    vite and MUI installed

--- Loan calculator/src/App.jsx
+++ Loan calculator/src/App.jsx
@@ -1,1 +1,30 @@
-rafce
+import React from 'react'
+import Home from './components/Home'
+import Exchange from './components/Exchange'
+import About from './components/About'
+import Error from './components/ErrorPage'
+
+const App = () => {
+  return (
+    <>
+    <div className='Lcontainer'>
+
+      {/* header: name of the app */}
+      <div className='Lheader'>
+        <h3> Loan Calculator</h3>
+      </div>
+
+      {/* the components */}
+      <div className='components'>
+        <Home />
+        <Exchange />
+        <About />
+        <Error />
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
