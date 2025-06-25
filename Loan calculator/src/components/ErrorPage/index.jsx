5776d1ee841a57ffe5f8731b9548d225464fcfcf
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-06-25 17:30:14 +0530

    Components and error page is changed

--- Loan calculator/src/components/ErrorPage/index.jsx
+++ Loan calculator/src/components/ErrorPage/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Home from '../Home'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div>
+      <h1>Something went wrong in the application.</h1>
+      <Link to={<Home />}> Go Home</Link>
+    </div>
   )
 }
 
---
+++ Loan calculator/src/components/ToggleTheme/contextTheme.jsx
@@ -0,0 +1,32 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { createContext, useContext, useMemo, useState } from "react";
+
+const ColorModeContext = createContext({ toggleColorMode: ()=> {} });
+
+export const useColorMode = () => useContext(ColorModeContext);
+
+
+
+export const ThemeContextProvider = ({ children }) => {
+    const [mode, setMode] = useState('light');
+
+    const colorMode = useMemo(()=>({
+        toggleColorMode: () => setMode((prev)=> (prev === 'light' ? 'dark' : 'light')),
+    }), []);
+
+    const theme = useMemo(()=> createTheme({
+        palette: {
+            mode,
+        },
+    }), [mode]);
+
+
+    return(
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
+};
