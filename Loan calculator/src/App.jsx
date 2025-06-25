ee1731ffe04008d279219c99738f9ed9c10efa28
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-06-25 14:40:19 +0530

    Router and Material UI used

--- Loan calculator/src/App.jsx
+++ Loan calculator/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Home from './components/Home'
 import Exchange from './components/Exchange'
 import About from './components/About'
@@ -6,24 +6,7 @@ import Error from './components/ErrorPage'
 
 const App = () => {
   return (
-    <>
-    <div className='Lcontainer'>
-
-      {/* header: name of the app */}
-      <div className='Lheader'>
-        <h3> Loan Calculator</h3>
-      </div>
-
-      {/* the components */}
-      <div className='components'>
-        <Home />
-        <Exchange />
-        <About />
-        <Error />
-      </div>
-
-    </div>
-    </>
+    <Router
   )
 }
 
