a30dc8c80d6962c56b849419820511bc63e242b1
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-06-25 20:27:28 +0530

    Header attached

--- Loan calculator/src/App.jsx
+++ Loan calculator/src/App.jsx
@@ -3,23 +3,16 @@ import Home from './components/Home'
 import Exchange from './components/Exchange'
 import About from './components/About'
 import Error from './components/ErrorPage'
+
 import { Route, Routes } from "react-router-dom"
 import ToggleTheme from './components/ToggleTheme'
 
 const App = () => {
   return (
-    <div>
-    <h2>Loan Calculator</h2>
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/exchange' element={<Exchange />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-
-       {/* theme toggle */}
-      <ToggleTheme />
-    </div>
+    <>
+    <h3>Loan Calculator</h3>
+    <Header />
+    </>
     
   )
 }
